ce3bc838fce6a52451a96903f63eb4c0f97d5d4c
Pablo Alvarez Gonzalez <pagonzalez@izertis.com>
2023-07-04 13:52:54 +0200

    Inicio del proyecto

---
+++ src/main/java/guru/springframework/jms/SfgJmssApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgJmssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgJmssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/jms/SfgJmssApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJmssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
